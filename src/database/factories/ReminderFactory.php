ba2c2ff0d76a1cd17d25330e0487e40112f28317
mirzapurnandi <mirza.purnandi@gmail.com>
2023-12-13 17:24:49 +0700

    add unitTest

---
+++ src/database/factories/ReminderFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reminder>
+ */
+class ReminderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->title(),
+            'description' => fake()->paragraph(),
+            'remind_at' => 1901246728,
+            'event_at' => 2101246733
+        ];
+    }
+}
