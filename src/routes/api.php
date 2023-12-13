fe58463951437c48a08a139f1f0b1cb2ae34cacb
mirzapurnandi <mirza.purnandi@gmail.com>
2023-12-13 11:24:54 +0700

    add Modul Reminders

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ReminderController;
 
 Route::get('/', function () {
     return response()->json([
@@ -15,4 +16,10 @@ Route::post('session', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::put('session', [AuthController::class, 'refreshToken']);
+
+    Route::get('reminders', [ReminderController::class, 'index']);
+    Route::post('reminders', [ReminderController::class, 'create']);
+    Route::get('reminders/{id}', [ReminderController::class, 'view']);
+    Route::put('reminders/{id}', [ReminderController::class, 'update']);
+    Route::delete('reminders/{id}', [ReminderController::class, 'destroy']);
 });
