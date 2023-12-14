4b1f0f5dfce1fb64eadd9377ac92f9b68afcb0a7
mirzapurnandi <mirza.purnandi@gmail.com>
2023-12-14 14:31:03 +0700

    add Frontend CORE

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <app-header />
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from './components/layouts/Header.vue'
+
+export default {
+    components: {
+        'app-header': Header
+    }
+}
+</script>
